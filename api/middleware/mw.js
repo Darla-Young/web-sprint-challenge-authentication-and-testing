ebf0aaebf04285b8d2320f4b74501bf32e04b4c7
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-22 14:41:45 -0600

    middleware drafted

--- api/middleware/mw.js
+++ api/middleware/mw.js
@@ -1,24 +1,21 @@
 const { findUser } = require('../auth/auth-model')
 
-function validateReq (req, res, next) {
+async function validateReq (req, res, next) {
   const { username, password } = req.body
 
-  if (!username || !password) res.status(400).json({message: 'username and password required'})
-  else {
-    console.log(req.originalUrl)
-    next()
+  if (!username || !password) {
+    res.status(400).json({message: 'username and password required'})
   }
-// if (req.originalUrl === {... '/login'})
-
-/*
-  FAILED registration
-    `username` exists
-    message: "username taken"
-
-  FAILED login
-    `username` does not exist || `password` is incorrect
-    message: "invalid credentials"
-*/
+  else {
+    const check = await findUser(username)
+    if (req.originalUrl === '/api/auth/login' && !check.password) {
+      res.status(401).json({message: 'invalid credentials'})
+    }
+    else if (req.originalUrl === '/api/auth/register' && check.username) {
+      res.status(409).json({message: 'username taken'})
+    }
+    else next()
+  }
 }
 
 module.exports = {
